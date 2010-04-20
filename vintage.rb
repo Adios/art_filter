609676b77dde960f1a7b4b922d9fe22a96fe6cd3
Adios <adiosf6f+git@gmail.com>
2010-04-20 00:58:18 +0000

    let it be in a module.

--- vintage.rb
+++ vintage.rb
@@ -12,30 +12,36 @@
 require 'rubygems'
 require 'RMagick'
 
-def vintager src, dest, opts = {}
-  include Magick
+module ArtFilter
+  module Vintage
 
-  opacity = { :yellow => 59, :magenta => 20, :cyan => 17 }.merge(opts)
+    def self.vintager src, dest, opts = {}
+      include Magick
 
-  src = Image.read(src).first
-  bleach = src.quantize(256, GRAYColorspace).gaussian_blur(1, 1).unsharp_mask(1, 1, 1, 0)
-  yellow = Image.new(src.columns, src.rows) { self.background_color = 'rgb(251, 242, 163)' }
-  magenta = Image.new(src.columns, src.rows) { self.background_color = 'rgb(232, 101, 179)' }
-  cyan= Image.new(src.columns, src.rows) { self.background_color = 'rgb(9, 73, 233)' }
+      opacity = { :yellow => 59, :magenta => 20, :cyan => 17 }.merge(opts)
 
-  yellow.opacity = QuantumRange * (1 - opacity[:yellow] / 100.0)
-  magenta.opacity = QuantumRange * (1 - opacity[:magenta] / 100.0)
-  cyan.opacity = QuantumRange * (1 - opacity[:cyan] / 100.0)
+      src = Image.read(src).first
+      bleach = src.quantize(256, GRAYColorspace).gaussian_blur(1, 1).unsharp_mask(1, 1, 1, 0)
+      yellow = Image.new(src.columns, src.rows) { self.background_color = 'rgb(251, 242, 163)' }
+      magenta = Image.new(src.columns, src.rows) { self.background_color = 'rgb(232, 101, 179)' }
+      cyan= Image.new(src.columns, src.rows) { self.background_color = 'rgb(9, 73, 233)' }
 
-  src.composite(bleach, 0, 0, OverlayCompositeOp).
-    composite(yellow, 0, 0, MultiplyCompositeOp).
-    composite(magenta, 0, 0, ScreenCompositeOp).
-    composite(cyan, 0, 0, ScreenCompositeOp).
-    write(dest) { self.quality = 100 }
+      yellow.opacity = QuantumRange * (1 - opacity[:yellow] / 100.0)
+      magenta.opacity = QuantumRange * (1 - opacity[:magenta] / 100.0)
+      cyan.opacity = QuantumRange * (1 - opacity[:cyan] / 100.0)
+
+      src.composite(bleach, 0, 0, OverlayCompositeOp).
+        composite(yellow, 0, 0, MultiplyCompositeOp).
+        composite(magenta, 0, 0, ScreenCompositeOp).
+        composite(cyan, 0, 0, ScreenCompositeOp).
+        write(dest) { self.quality = 100 }
+    end
+
+  end
 end
 
 if __FILE__ == $0
-  vintager 'tomato.jpg', 'result.jpg'
+  ArtFilter::Vintage.vintager 'tomato.jpg', 'result.jpg'
 end
 
 # vim:sts=2 sw=2 expandtab:
